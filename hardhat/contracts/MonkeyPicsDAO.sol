dcef430e489ee19e76b9fb112a30bd8ad06daf61
Giulio de Cadilhac <g.decadilhac@gmail.com>
2023-08-24 17:22:04 +0200

    dao contract done, functions: create, vote, execute proposals, Owner withdraw eth, receive and fallback

--- hardhat/contracts/MonkeyPicsDAO.sol
+++ hardhat/contracts/MonkeyPicsDAO.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface IFakeNFTMarketplace {
+    function purchase(uint256 _tokenId) external payable;
+
     function getPrice() external view returns (uint256);
 
     function available(uint256 _tokenId) external view returns (bool);
-
-    function purchase(uint256 _tokenId) external payable;
 }
 
 interface IMonkeyPicsNFT {
@@ -56,4 +56,82 @@ contract MonkeyPicsDAO is Ownable {
         numProposalsCreated++;
         return numProposalsCreated - 1;
     }
+
+    modifier activeProposalOnly(uint256 proposalIndex) {
+        require(
+            tokenIdToProposals[proposalIndex].deadline > block.timestamp,
+            "DEADLINE_EXCEEDED"
+        );
+        _;
+    }
+
+    enum Vote {
+        Y, // Y = 0
+        N // N = 1
+    }
+
+    function voteOnProposal(
+        uint256 proposalIndex,
+        Vote vote
+    ) external nftHolderOnly activeProposalOnly(proposalIndex) {
+        Proposal storage proposal = tokenIdToProposals[proposalIndex];
+
+        uint256 voterNFTBalance = monkeyPicsNFT.balanceOf(msg.sender);
+        uint256 numVotes = 0;
+
+        // Calculate how many NFTs are owned by the voter that haven't already been used for voting on this proposal
+        for (uint256 i = 0; i < voterNFTBalance; i++) {
+            uint256 tokenId = monkeyPicsNFT.tokenOfOwnerByIndex(msg.sender, i);
+            if (proposal.tokenIdToVoted[tokenId] == false) {
+                numVotes++;
+                proposal.tokenIdToVoted[tokenId] = true;
+            }
+        }
+        require(numVotes > 0, "ALREADY_VOTED");
+
+        if (vote == Vote.Y) {
+            proposal.yVotes += numVotes;
+        } else {
+            proposal.nVotes += numVotes;
+        }
+    }
+
+    modifier inactiveProposalOnly(uint256 proposalIndex) {
+        require(
+            tokenIdToProposals[proposalIndex].deadline <= block.timestamp,
+            "DEADLINE_NOT_EXCEEDED"
+        );
+        require(
+            tokenIdToProposals[proposalIndex].executed == false,
+            "PROPOSAL_ALREADY_EXECUTED"
+        );
+        _;
+    }
+
+    // any NFT holder can execute a proposal after it's deadline
+    function executeProposal(
+        uint256 proposalIndex
+    ) external nftHolderOnly inactiveProposalOnly(proposalIndex) {
+        Proposal storage proposal = tokenIdToProposals[proposalIndex];
+
+        // If the proposal passes, purchase the NFT from the FakeNFTMarketplace
+        if (proposal.yVotes > proposal.nVotes) {
+            uint256 nftPrice = nftMarketplace.getPrice();
+            require(address(this).balance >= nftPrice, "NOT_ENOUGH_FUNDS");
+            nftMarketplace.purchase{value: nftPrice}(proposal.nftTokenId);
+        }
+        proposal.executed = true;
+    }
+
+    function withdrawEther() external onlyOwner {
+        uint256 amount = address(this).balance;
+        require(amount > 0, "Nothing to withdraw, contract balance empty");
+        (bool sent, ) = payable(owner()).call{value: amount}("");
+        require(sent, "FAILED_TO_WITHDRAW_ETHER");
+    }
+
+    // The following 2 functions allow the contract to accept ETH deposit directly from a wallet without calling a function
+    receive() external payable {}
+
+    fallback() external payable {}
 }
